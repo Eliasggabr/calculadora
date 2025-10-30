4c3c8555b3cdf5a3c69115bdbbac6bc93ac7ad5f
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2025-10-30 09:10:31 -0300

    Implement book addition in adicionar_livro function
    Add functionality to input book details and append to list

--- bibliotecaPython/funcoes.py
+++ bibliotecaPython/funcoes.py
@@ -4,7 +4,8 @@ def adicionar_livro(listaLivros):
     titulo = input("Por favor, digite o título do livro: ")
     autor = input("Por favor, digite o autor do livro: ")
     status = "Disponível"
-
+    dicionario = {"Título": titulo, "Autor": autor, "Status": status}
+    listaLivros.append(dicionario)
 
 def emprestar_livro(listaLivros):
     status = "Emprestado"
