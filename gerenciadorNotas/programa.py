fb9172604d780dbc10b8c496811af4548bdb2c08
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2025-11-03 16:11:51 -0300

    Update programa.py

--- gerenciadorNotas/programa.py
+++ gerenciadorNotas/programa.py
@@ -1,14 +1,22 @@
+lfrom funcoes import *
+
 listaNotas = []
-listaAlunos = []
+listaAlunos = {}
 
 while True:
-    op = int(input("Digite a operação, 1 para adicionar um aluno, 2 para exibir relatório, 0 para sair: "))
+    op = int(input("Digite 1 para adicionar um aluno, 2 para mostrar o relatório, 0 para sair: "))
     if op == 1:
         nome = input("Digite um nome: ")
         for i in range(1,4):
             nota = float(input(f"Digite a {i} nota do aluno: "))
             listaNotas.append(nota)
-            media = calcularMedia(listaNotas)
-            status = verificacao(media)
-            dictAluno = { nome: [listaNotas, media, status]}
-            
+        media = calcularMedia(listaNotas)
+        status = vericacao(media)
+        listaAlunos = {nome: [listaNotas, media, status]}
+       
+    if op == 2:
+        print(listaAlunos)
+    if op == 0:
+        print("Encerrando...")
+        break
+
