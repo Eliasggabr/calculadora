c0ca015e35371e7a4401767aca2c9d67b571b5c1
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2025-11-17 11:12:20 -0300

    Initialize programa.py with menu structure

---
+++ programa.py
@@ -0,0 +1,8 @@
+from funcoes import *
+
+Cardapio = []
+Pedido = []
+
+while True:
+    print("1 - Ver cardápio \n2 - Adicionar item ao pedido \n3 - Ver pedido \n4 - Remover item \n0 - Finalizar")
+    
