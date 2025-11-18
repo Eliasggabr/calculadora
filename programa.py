962bde363eded2b3604251f26c4fda6cd3e1d092
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2025-11-18 10:23:34 -0300

    Add menu options for order management

--- programa.py
+++ programa.py
@@ -3,6 +3,19 @@ from funcoes import *
 Cardapio = []
 Pedido = []
 
+carregar_cardapio(Cardapio)
+
 while True:
     print("1 - Ver cardápio \n2 - Adicionar item ao pedido \n3 - Ver pedido \n4 - Remover item \n0 - Finalizar")
-    
+    op = int(input("Digite o número da operação desejada: "))
+    if op == 1:
+        exibir_cardapio(Cardapio)
+    elif op == 2:
+        adicionar_pedido(Cardapio, Pedido)
+    elif op == 3:
+        exibir_pedido(Pedido)
+    elif op == 4:
+        remover_item(Pedido)
+    elif op == 0:
+        print("Finalizando...")
+        break
